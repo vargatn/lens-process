0c5e183aaf5d697d57ead69560c6ae15575ea576
Tamas Norbert Varga <vargatn@gmail.com>
2017-01-11 15:18:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Location for the cluster lensing measurement module
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="clustlens",
+      description="Data preparation and analysis functions for DES Y1 cluster lensing",
+      packages = find_packages(),
+      author="The DES Y1 cluster lensing group",
+      author_email="vargat@gmail.com",
+      version="0.0")
