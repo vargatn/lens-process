8a19d8c1bef6b5adde0666de102dc510d53119f8
Tamas Norbert Varga <vargatn@gmail.com>
2017-01-11 16:01:10 +0100

    updated gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name="clustlens",
-      description="Data preparation and analysis functions for DES Y1 cluster lensing",
+setup(name="proclens",
+      description="Data processing preparation and analysis functions for DES Y1 cluster lensing",
       packages = find_packages(),
       author="The DES Y1 cluster lensing group",
       author_email="vargat@gmail.com",
