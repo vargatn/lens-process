3bf262cf58abafc37b7077e682c5b740f1c8d43a
Tamas Norbert Varga <vargatn@gmail.com>
2017-04-30 23:40:54 +0200

    fixed magnitude error

--- proclens/io/catalogs.py
+++ proclens/io/catalogs.py
@@ -6,26 +6,16 @@ FITS tables sometimes have multidimensional columns, which are not supported for
 Pandas DataFrames however provide many nice features, such as SQL speed database matchings
 """
 
-
-
 import numpy as np
 import pandas as pd
-import esutil.numpy_util as nu
-import copy
-from astropy.table import Table
 
 
 def flat_type(recarr):
-    mags = ['G', 'R', 'I', 'Z']
-
     newtype = []
     for dt in recarr.dtype.descr:
         if len(dt) == 3:
             for i in np.arange(dt[2][0]):
-                if 'MAG' in dt[0]:
-                    newtype.append((dt[0] + '_' + mags[i], dt[1]))
-                else:
-                    newtype.append((dt[0] + '_' + str(i), dt[1]))
+                newtype.append((dt[0] + '_' + str(i), dt[1]))
         else:
             newtype.append(dt)
     return newtype
@@ -52,6 +42,7 @@ def flat_copy(recarr):
 
 
 def to_pandas(recarr):
+    """Converts potentially nested Fits Table into Pandas DataFrame"""
     newarr = flat_copy(recarr)
     res = pd.DataFrame.from_records(newarr.byteswap().newbyteorder(), columns=newarr.dtype.names)
     return res
